1b87163d3e4352f824df4584bd672d7bcf7cfc10
techweed <josephantonykj006@gmail.com>
2021-12-19 01:35:26 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Helgdfgdfhgdlxvfgo</div>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
